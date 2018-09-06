d7474efc1a8a9e794dafd9baa2709fb6e6aa7b87
Ryan <rwiesenberg603@gmail.com>
2018-09-05 21:22:10 -0400

    trying bettwe arrows

--- assets/slides.js
+++ assets/slides.js
@@ -6,7 +6,7 @@ const images = [
 
 function increaseSideIndex() {
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
+    if (slideIndex >= images.length) {slideIndex = 0}
     setSlideIndex(slideIndex)
 }
 
